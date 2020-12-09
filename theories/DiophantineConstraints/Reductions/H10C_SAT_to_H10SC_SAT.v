0b4d8b2a88e0148defc758c41f6886d26a1e6999
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2020-12-09 17:56:48 +0100

    Finish reduction from H10C to H10CS

--- theories/DiophantineConstraints/Reductions/H10C_SAT_to_H10SC_SAT.v
+++ theories/DiophantineConstraints/Reductions/H10C_SAT_to_H10SC_SAT.v
@@ -4,9 +4,8 @@ Import ListNotations.
 
 Require Import Undecidability.DiophantineConstraints.H10C.
 
-Module Argument.
 
-Section ListFacts.
+Module Argument.
 
 Lemma Forall_cons_iff {T : Type} {P} : forall (l : list T) (a : T), Forall P (a :: l) <-> P a /\ Forall P l.
 Proof.
@@ -14,6 +13,8 @@ Proof.
   - now inversion H.
   - now constructor.
 Qed.
+
+(* Forall_app l1 l2 : Forall (l1 ++ l2) <-> Forall l1 /\ Forall l2*)
 
 Lemma Forall_flat_map_iff {T U : Type} {P} {ds : list U} {f : U -> list T} :
   Forall P (flat_map f ds) <-> Forall (fun d => Forall P (f d)) ds.
@@ -27,153 +28,150 @@ Proof.
     + apply Forall_cons_iff in H. apply Forall_app. firstorder.
 Qed.
 
-End ListFacts.
-
-Definition sat_sc psi l := Forall (h10sc_sem psi) l.
-
-Lemma sat_sc_app {psi l1 l2} : sat_sc psi (l1 ++ l2) <-> sat_sc psi l1 /\ sat_sc psi l2.
-Proof.
-  unfold sat_sc. apply Forall_app.
-Qed.
-
-Lemma sat_sc_cons {psi c l} : sat_sc psi (c :: l) <-> h10sc_sem psi c /\ sat_sc psi l.
-Proof.
-  unfold sat_sc. apply Forall_cons_iff.
-Qed.
-
-Lemma sat_sc_singleton {psi c} : sat_sc psi [c] <-> h10sc_sem psi c.
-Proof.
-  unfold sat_sc. split; intros.
-  - now apply Forall_cons_iff in H.
-  - now apply Forall_cons_iff.
-Qed.
-
-Definition compatible (f psi : nat -> nat) l := forall i, nth i l (psi (f i)) = psi (f i).
-
-Lemma compatible_cons {f psi n l} :
-  compatible f psi (n :: l) <-> psi (f 0) = n /\ compatible (fun i => f (1 + i)) psi l.
-Proof.
-  split.
-  - constructor.
-    + specialize (H 0). simpl in H. now rewrite H.
-    + intros i. specialize (H (1 + i)). now apply H.
-  - intros [H1 H2]. unfold compatible. intros [ |i].
-    + now simpl.
-    + simpl. now apply H2.
-Qed.
-
-Lemma compatible_app {f psi l1 l2} :
-  compatible f psi (l1 ++ l2) <-> compatible f psi l1 /\ compatible (fun i => f (length l1 + i)) psi l2.
-Proof.
-  split.
-  - induction l1.
-    + simpl. intros H. split.
-      * intros [|i]; reflexivity.
-      * apply H.
-    + intros H. simpl app in H. apply compatible_cons in H. split.
-      * intros i. induction i.
-        -- now simpl.
-        -- Admitted.
-
 (* bijection from nat * nat to nat using the Cantor pairing function *)
 Definition encode '(x, y) : nat :=
-  (x + y) * (x + y + 1) / 2 + y.
+  y + (nat_rec _ 0 (fun i m => (S i) + m) (y + x)).
 
 (* bijection from nat to nat * nat *)
 Definition decode (n : nat) : nat * nat :=
-  let w := (Nat.sqrt (8 * n + 1) - 1) / 2 in
-  let t := (w * w + w) / 2 in
-  let y := n - t in
-  (w - y, y).
+  nat_rec _ (0, 0) (fun _ '(x, y) => match x with O => (S y, 0) | S x => (x, S y) end) n.
 
 Lemma decode_encode {z : nat * nat} : decode (encode z) = z.
-Admitted.
-
-Definition express_one (x : nat) (f : nat -> nat) :=
-  [h10sc_one x].
-
-Lemma express_one_elim x f psi : sat_sc psi (express_one x f) -> psi x = 1.
 Proof.
-  intros H. now apply sat_sc_singleton in H.
+  remember (encode z) eqn:H. revert z H. induction n as [|n IH].
+  - destruct z as [[|?] [|?]]; now simpl.
+  - destruct z as [x y]. destruct y; simpl.
+    + destruct x; simpl; try congruence; intros [=].
+      rewrite (IH (0, x)); simpl; try reflexivity.
+      now rewrite H0, Nat.add_0_r.
+    + intros [=]. rewrite (IH (S x, y)); simpl; try reflexivity.
+      now rewrite Nat.add_succ_r, H0.
 Qed.
 
-Definition express_plus (x y z : nat) (f : nat -> nat) :=
-  [h10sc_plus x y z].
+Opaque encode decode.
 
-Lemma express_plus_elim x y z f psi : sat_sc psi (express_plus x y z f) -> psi x + psi y = psi z.
-Proof.
-  intros H. now apply sat_sc_singleton in H.
-Qed.
+Section Reduction.
 
-(* needs 7 fresh variables *)
+Context (cs : list h10c).
+
+(* Original variable renaming *)
+Definition tau (x : nat) := encode (x, 0).
+
+(* Mapped variable renaming *)
+Definition theta (x y t : nat) := encode (x, 1 + encode (y, t)).
+
+(* needs 6 fresh variables *)
+(* x^2        -> theta x y 0
+   y^2        -> theta x y 1
+   2z         -> theta x y 2
+   x + y      -> theta x y 3
+   (x + y)^2  -> theta x y 4
+   x^2 + y^2  -> theta x y 5 *)
 (* x * y = z if
-   z + z + y * y + x * x = (x + y) * (x + y)*)
+   2z + x^2 + y^2 = (x + y)^2 *)
+Definition h10c_to_h10cs (c : h10c) : list h10sc :=
+  match c with
+  | h10c_one x => [h10sc_one (tau x)]
+  | h10c_plus x y z => [h10sc_plus (tau x) (tau y) (tau z)]
+  | h10c_mult x y z => [
+      h10sc_sqr (tau x) (theta x y 0);
+      h10sc_sqr (tau y) (theta x y 1);
+      h10sc_plus (tau z) (tau z) (theta x y 2);
+      h10sc_plus (tau x) (tau y) (theta x y 3);
+      h10sc_sqr (theta x y 3) (theta x y 4);
+      h10sc_plus (theta x y 0) (theta x y 1) (theta x y 5);
+      h10sc_plus (theta x y 2) (theta x y 5) (theta x y 4)]
+  end.
 
-Definition express_mult (x y z : nat) (f : nat -> nat) :=
-  [h10sc_plus z z (f 2); h10sc_sqr y (f 1); h10sc_sqr x (f 0); h10sc_plus (f 2) (f 1) (f 3);
-   h10sc_plus (f 3) (f 0) (f 4); h10sc_plus x y (f 5); h10sc_sqr (f 5) (f 4)].
+Definition scs := flat_map h10c_to_h10cs cs.
 
-Lemma express_mult_elim x y z f psi : sat_sc psi (express_mult x y z f) -> psi x * psi y = psi z.
+Section Transport.
+
+(* Solution to a given set of H10C *)
+Context (phi : nat -> nat) (H : forall c, In c cs -> h10c_sem c phi).
+
+(* Construct a solution to a given set of H10SC given a solution of H10C *)
+Definition psi (n : nat) :=
+  match decode n with
+  | (x, 0) => phi x
+  | (x, S m) =>
+    match decode m with
+    | (y, 0) => (phi x) * (phi x)
+    | (y, 1) => (phi y) * (phi y)
+    | (y, 2) => ((phi x) * (phi y)) + ((phi x) * (phi y))
+    | (y, 3) => (phi x) + (phi y)
+    | (y, 4) => ((phi x) + (phi y)) * ((phi x) + (phi y))
+    | (y, 5) => ((phi x) * (phi x)) + ((phi y) * (phi y))
+    | (_, _) => 0
+    end
+  end.
+
+Arguments psi /.
+Arguments tau /.
+Arguments theta /.
+
+Lemma h10c_to_h10sc_spec {c} : h10c_sem c phi -> Forall (h10sc_sem psi) (h10c_to_h10cs c).
 Proof.
-  intros H.
-  apply Forall_cons_iff in H as [H1 H].
-  apply Forall_cons_iff in H as [H2 H].
-  apply Forall_cons_iff in H as [H3 H].
-  apply Forall_cons_iff in H as [H4 H].
-  apply Forall_cons_iff in H as [H5 H].
-  apply Forall_cons_iff in H as [H6 H7].
-  apply sat_sc_singleton in H7.
-  unfold h10sc_sem in H1, H2, H3, H4, H5, H6, H7.
+  destruct c; simpl.
+  - intros H1. constructor; simpl; try trivial.
+    now rewrite decode_encode.
+  - intros H1. constructor; try trivial.
+    simpl. now repeat rewrite decode_encode.
+  - intros H1. repeat constructor; repeat (simpl; rewrite decode_encode); nia.
+Qed.
+
+End Transport.
+
+Lemma transport : H10C_SAT cs -> H10SC_SAT scs.
+Proof.
+  intros [phi H]. exists (psi phi).
+  revert H. repeat rewrite <- Forall_forall.
+  unfold scs.
+  intros H. apply Forall_flat_map_iff.
+  revert H. apply Forall_impl.
+  apply h10c_to_h10sc_spec.
+Qed.
+
+Section Reflection.
+
+Context (psi : nat -> nat) (H : forall c, In c scs -> h10sc_sem psi c).
+
+Definition phi (n : nat) := psi (tau n).
+
+Lemma h10sc_to_h10c_spec {c} : Forall (h10sc_sem psi) (h10c_to_h10cs c) -> h10c_sem c phi.
+Proof.
+  destruct c; simpl; try (intros H1; apply Forall_cons_iff in H1; now simpl in H1). intros H1.
+  unfold phi.
+  apply Forall_cons_iff in H1 as [H1 H2].
+  apply Forall_cons_iff in H2 as [H2 H3].
+  apply Forall_cons_iff in H3 as [H3 H4].
+  apply Forall_cons_iff in H4 as [H4 H5].
+  apply Forall_cons_iff in H5 as [H5 H6].
+  apply Forall_cons_iff in H6 as [H6 H7].
+  apply Forall_cons_iff in H7 as [H7 H8].
+  unfold h10sc_sem in H1.
+  unfold h10sc_sem in H2.
+  unfold h10sc_sem in H3.
+  unfold h10sc_sem in H4.
+  unfold h10sc_sem in H5.
+  unfold h10sc_sem in H6.
+  unfold h10sc_sem in H7.
   nia.
 Qed.
 
-Definition embed '(x, y, z, i, j) : nat :=
-  encode (encode (encode (encode (x, y), z), i), j).
+End Reflection.
 
-Definition unembed (n : nat) :=
-  let (xyzi, j) := decode n in
-  let (xyz, i) := decode xyzi in
-  let (xy, z) := decode xyz in
-  let (x, y) := decode xy in
-  (x, y, z, i, j).
-
-Lemma unembed_embed {c : nat * nat * nat * nat * nat} : unembed (embed c) = c.
+Lemma reflection : H10SC_SAT scs -> H10C_SAT cs.
 Proof.
-  destruct c as [[[[? ?] ?] ?] ?].
-  unfold embed. unfold unembed.
-  now repeat rewrite decode_encode.
+  intros [psi H]. exists (phi psi).
+  revert H. repeat rewrite <- Forall_forall.
+  unfold scs.
+  intros H. apply Forall_flat_map_iff in H.
+  revert H. apply Forall_impl.
+  apply h10sc_to_h10c_spec.
 Qed.
 
-Definition h10c_to_h10sc (c : h10c) : list h10sc :=
-  let h x := embed (x, 0, 0, 0, 0) in
-  match c with
-    | h10c_one x => express_one (h x) (fun i => embed (x, 0, 0, 1, i))
-    | h10c_plus x y z => express_plus (h x) (h y) (h z) (fun i => embed (x, y, z, 2, i))
-    | h10c_mult x y z => express_mult (h x) (h y) (h z) (fun i => embed (x, y, z, 3, i))
-  end.
-
-Lemma transport (l : list h10c) : H10C_SAT l -> H10SC_SAT (flat_map h10c_to_h10sc l).
-Admitted.
-
-Lemma reflection (l : list h10c) : H10SC_SAT (flat_map h10c_to_h10sc l) -> H10C_SAT l.
-Proof.
-  intros [psi Hpsi]. apply Forall_forall in Hpsi.
-  pose (h x := embed (x, 0, 0, 0, 0)).
-  pose (phi x := psi (h x)).
-  exists phi. induction l; revert Hpsi.
-  - firstorder.
-  - simpl flat_map. intros Hpsi. apply Forall_app in Hpsi. intros c [H1|H2].
-    + destruct c.
-      * rewrite H1 in Hpsi. apply (express_one_elim (h n) (fun i => embed (n, 0, 0, 1, i))).
-        apply Hpsi.
-      * rewrite H1 in Hpsi.
-        apply (express_plus_elim (h n) (h n0) (h n1) (fun i => embed (n, n0, n1, 2, i))).
-        apply Hpsi.
-      * rewrite H1 in Hpsi.
-        apply (express_mult_elim (h n) (h n0) (h n1) (fun i => embed (n, n0, n1, 3, i))).
-        apply Hpsi.
-    + now apply IHl.
-Qed.
+End Reduction.
 
 End Argument.
 
@@ -181,7 +179,7 @@ Require Import Undecidability.Synthetic.Definitions.
 
 Theorem reduction : reduces H10C_SAT H10SC_SAT.
 Proof.
-  exists (flat_map Argument.h10c_to_h10sc).
+  exists (fun cs => Argument.scs cs).
   intro l. constructor.
   - exact (Argument.transport l).
   - exact (Argument.reflection l).
